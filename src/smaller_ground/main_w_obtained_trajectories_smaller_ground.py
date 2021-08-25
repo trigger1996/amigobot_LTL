e8c08dd6c29747dd169db4455d53709862b6b41e
trigger1996 <529388771@qq.com>
2021-08-25 18:34:04 +0800

    updated map of smaller ground

--- src/smaller_ground/main_w_obtained_trajectories_smaller_ground.py
+++ src/smaller_ground/main_w_obtained_trajectories_smaller_ground.py
@@ -12,8 +12,8 @@ import bot_libs.bot_ts as bot_ts
 # uncomment the correspongding case to represent run by amigobot
 global time_to_wait, v_max
 time_to_wait = 10        # seconds
-v_max_1_2 = 0.095        # m/s
-v_max_3   = 0.095        # 0.245 m/s if craching        default: 0.205 m/s
+v_max_1_2 = 0.195        # m/s
+v_max_3   = 0.195        # 0.245 m/s if craching        default: 0.205 m/s
 
 
 def calculate_final_time(bot_w_ts):
@@ -68,10 +68,9 @@ def main():
                                                    time_to_wait = time_to_wait, u_dist_max = v_max_3)
     '''
     # CASE 3 INV LARGER
-    '''
     prefixes = [['u1', 'u1'],
                 ['u2', 'u2'],
-                '23']]
+                ['23']]
     suffix_cycles = [['27', '28', 'g4', '28', '27', 'u1', '27', '28', '28', '28', '28', '28', 'g4', '28', '27', 'u1', '27', '28', 'g4', '28', '27', 'u1', 'u1', 'u1', 'u1', 'u1', '27', '28', 'g4', '28', '27', 'u1'],
                      ['23', '24', 'g2', '24', '23', 'u2', 'u2', 'u2', 'u2', 'u2', '23', '24', 'g2', '24', '23', 'u2', '23', '24', 'g2', '24', '23', 'u2', '23', '24', '24', '24', '24', '24', 'g2', '24', '23', 'u2'],
                      ['22', '21', '28', '27', '26', '25', '24', '23', '22', '21', '28', '27', '26', '25', '24', '23', '22', '21', '28', '27', '26', '25', '24', '23']]
@@ -85,10 +84,11 @@ def main():
     bot_3 = bot_ts.turtlebot_TS(name='amigobot_3', yaml_file='/home/ubuntu484/catkin_ws/src/amigobot_LTL/model/ijrr2013_smaller_ground/3_vehicles/robot_3_inv_larger.yaml',   # robot_3_inv_larger.yaml   robot_3_inv.yaml
                                                    map_file ='/home/ubuntu484/catkin_ws/src/amigobot_LTL/model/ijrr2013_smaller_ground/3_vehicles/map.yaml',
                                                    time_to_wait = time_to_wait, u_dist_max = v_max_3)
-    '''
+
 
     # CASE 4 INV LARGER
     # Check .yaml before launching
+    '''
     prefixes = [['u1', 'u1'],
                 ['u2', 'u2'],
                 ['23']]
@@ -105,7 +105,7 @@ def main():
     bot_3 = bot_ts.turtlebot_TS(name='amigobot_3', yaml_file='/home/ubuntu484/catkin_ws/src/amigobot_LTL/model/ijrr2013_smaller_ground/3_vehicles/robot_3_inv_larger.yaml',   # robot_3_inv_larger.yaml   robot_3_inv.yaml
                                                    map_file ='/home/ubuntu484/catkin_ws/src/amigobot_LTL/model/ijrr2013_smaller_ground/3_vehicles/map.yaml',
                                                    time_to_wait = time_to_wait, u_dist_max = v_max_3)
-
+    '''
     # robot_3_inv_larger.yaml
     # robot_3_inv.yaml
 
